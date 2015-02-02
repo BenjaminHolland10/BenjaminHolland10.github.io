4f07e136b448c0f948162cecc1feb24b611c6527
BenjaminHolland10 <bhollnd2@illinois.edu>
2015-02-02 00:06:51 -0600

    d
    d

--- good.js
+++ good.js
@@ -13,7 +13,7 @@ window.onload = function() {
      SC.initialize({      
         client_id: '0cf77f09d09ff38c8e3a2d7ef1b21e1b'
         });
-    var menuLinks = document.getElementsByClassName('genre')
+    var menuLinks = document.getElementsByClassName('genre');
     for (var i = 0; i< menuLinks.length; i++){
         var menuLink = menuLinks[i];
         menuLink[i].onclick = function(e){
