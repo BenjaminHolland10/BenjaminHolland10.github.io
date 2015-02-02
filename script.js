da22a9956d30c0d52d305ec196049ad9ce2e1966
BenjaminHolland10 <bhollnd2@illinois.edu>
2015-02-01 22:59:01 -0600

    d
    d

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 SC.initialize({
   client_id: '0cf77f09d09ff38c8e3a2d7ef1b21e1b',
+  redirect_uri: 'http://external.codecademy.com/soundcloud.html'
 });
 
-SC.get("/groups/55517/tracks", {limit: 1}, function(tracks){
-  alert("Latest track: " + tracks[0].title);
+$(document).ready(function() {
+  $('a.connect').click(function(e) {
+    e.preventDefault();
+    SC.connect(function() {
+      SC.get('/me', function(me) {
+        $('#username').html(me.username);
+      });
+    });
+  });
 });
