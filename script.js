4b1a5781c7db1566627213ba64ec08b24e935eff
BenjaminHolland10 <bhollnd2@illinois.edu>
2015-02-28 03:12:57 -0600

    g
    g

---
+++ script.js
@@ -0,0 +1,11 @@
+SC.initialize({
+  client_id: '0cf77f09d09ff38c8e3a2d7ef1b21e1b,
+  redirect_uri: 'http://benjaminholland10.github.io/callback.html'
+});
+
+$(document).ready(function() {
+  $('a.connect').click(function(e) {
+    e.preventDefault();
+    
+  });
+});
