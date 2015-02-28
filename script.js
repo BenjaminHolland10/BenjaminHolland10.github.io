3c567ff707fce8d22011c1852dcc48a5dd227b85
BenjaminHolland10 <bhollnd2@illinois.edu>
2015-02-28 03:18:48 -0600

    d
    d

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 SC.initialize({
-  client_id: '0cf77f09d09ff38c8e3a2d7ef1b21e1b,
+  client_id: '0cf77f09d09ff38c8e3a2d7ef1b21e1b',
   redirect_uri: 'http://benjaminholland10.github.io/callback.html'
 });
 
