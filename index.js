49deb052e61cd4732e6d41e29a3dd52ae8ee8ae1
BenjaminHolland10 <bhollnd2@illinois.edu>
2015-02-28 00:25:51 -0600

    d

--- index.js
+++ index.js
@@ -13,15 +13,12 @@ $(document).ready(function () {
     //circleColor: use @default
     data: {
       items: [
-        {text: "Java", count: "236"},
-        {text: ".Net", count: "382"},
-        {text: "Php", count: "170"},
-        {text: "Ruby", count: "123"},
-        {text: "D", count: "12"},
-        {text: "Python", count: "170"},
-        {text: "C/C++", count: "382"},
-        {text: "Pascal", count: "10"},
-        {text: "Something", count: "170"},
+        {text: "Varun", count: "236"},
+        {text: "Ben", count: "382"},
+        {text: "Jordan", count: "170"},
+        {text: "Ahilan", count: "123"},
+        {text: "Peter", count: "12"},
+        {text: "", count: "170"}
       ],
       eval: function (item) {return item.count;},
       classed: function (item) {return item.text.split(" ").join("");}
