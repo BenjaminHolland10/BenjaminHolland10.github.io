36aef93087ce40c3fc9fe0eb6a8d61ed35c4da1f
BenjaminHolland10 <bhollnd2@illinois.edu>
2015-02-28 01:19:37 -0600

    d
    d

--- index.js
+++ index.js
@@ -15,13 +15,7 @@ $(document).ready(function () {
       items: [
         {text: "Java", count: "236"},
         {text: ".Net", count: "382"},
-        {text: "Php", count: "170"},
-        {text: "Ruby", count: "123"},
-        {text: "D", count: "12"},
-        {text: "Python", count: "170"},
-        {text: "C/C++", count: "382"},
-        {text: "Pascal", count: "10"},
-        {text: "Something", count: "170"},
+        {text: "Php", count: "170"}
       ],
       eval: function (item) {return item.count;},
       classed: function (item) {return item.text.split(" ").join("");}
